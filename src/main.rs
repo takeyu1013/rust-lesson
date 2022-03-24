924312dff3a3f7a14c56dfdff1c80c64dc8f9255
takeyu1013 <yuto1013@gmail.com>
2022-03-24 23:44:26 +0900

    ✨

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 // mod vars;
 // mod stack_heap;
-mod ownership;
+// mod ownership;
+mod generics;
 
 fn main() {
     // println!("Hello, world!");
     // vars::run();
     // stack_heap::run();
-    ownership::run();
+    // ownership::run();
+    generics::run();
     // vars::sub_a::func_a();
     // vars::sub_b::func_b();
 }
